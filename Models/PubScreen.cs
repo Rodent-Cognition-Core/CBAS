6e71a14a51b431b389c2ea58ecad0a53eba4cc6f
mouse-ware <ezareei@uwo.ca>
2024-11-29 14:01:16 -0500

    Revert "null handling in object model"
    This reverts commit 67bef72a8d0e5d8c035412b59a79b40cfa1d15c1.

--- Models/PubScreen.cs
+++ Models/PubScreen.cs
@@ -1,54 +1,53 @@
 using Microsoft.AspNetCore.Identity;
 using System;
-using System.Collections.Generic;
 
 namespace AngularSPAWebAPI.Models
 {
     public class PubScreen
     {
-        public int? ID { get; set; } = null;
+        public int? ID { get; set; }
 
-        public Guid PaperLinkGuid { get; set; } = Guid.Empty;
-        public string Title { get; set; } = string.Empty;
-        public string Abstract { get; set; } = string.Empty;
-        public string Keywords { get; set; } = string.Empty;
-        public string DOI { get; set; } = string.Empty;
-        public string Year { get; set; } = string.Empty;
-        public int?[] YearID { get; set; } = new int?[0];
-        public int?[] AuthourID { get; set; } = new int?[0];
-        public string AuthorString { get; set; } = string.Empty;
-        public int? PaperTypeID { get; set; } = null;
-        public int?[] PaperTypeIdSearch { get; set; } = new int?[0];
-        public string PaperType { get; set; } = string.Empty;
-        public int?[] TaskID { get; set; } = new int?[0];
-        public int?[] SubTaskID { get; set; } = new int?[0];
-        public int?[] SpecieID { get; set; } = new int?[0];
-        public int?[] sexID { get; set; } = new int?[0];
-        public int?[] StrainID { get; set; } = new int?[0];
-        public int?[] DiseaseID { get; set; } = new int?[0];
-        public int?[] SubModelID { get; set; } = new int?[0];
-        public int?[] RegionID { get; set; } = new int?[0];
-        public int?[] SubRegionID { get; set; } = new int?[0];
-        public int?[] CellTypeID { get; set; } = new int?[0];
-        public int?[] MethodID { get; set; } = new int?[0];
-        public int?[] SubMethodID { get; set; } = new int?[0];
-        public int?[] TransmitterID { get; set; } = new int?[0];
-        public string Reference { get; set; } = string.Empty;
-        public string Source { get; set; } = string.Empty;
-        public int? YearFrom { get; set; } = null;
-        public int? YearTo { get; set; } = null;
-        public string TaskOther { get; set; } = string.Empty;
-        public string SpecieOther { get; set; } = string.Empty;
-        public string StrainMouseOther { get; set; } = string.Empty;
-        public string StrainRatOther { get; set; } = string.Empty;
-        public string DiseaseOther { get; set; } = string.Empty;
-        public string CelltypeOther { get; set; } = string.Empty;
-        public string MethodOther { get; set; } = string.Empty;
-        public string NeurotransOther { get; set; } = string.Empty;
-        public string search { get; set; } = string.Empty;
+        public Guid PaperLinkGuid { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Keywords { get; set; }
+        public string DOI { get; set; }
+        public string Year { get; set; }
+        public int?[] YearID { get; set; }
+        public int?[] AuthourID { get; set; }
+        public string AuthorString { get; set; }
+        public int? PaperTypeID { get; set; }
+        public int?[] PaperTypeIdSearch { get; set; }
+        public string PaperType { get; set; }
+        public int?[] TaskID { get; set; }
+        public int?[] SubTaskID { get; set; }
+        public int?[] SpecieID { get; set; }
+        public int?[] sexID { get; set; }
+        public int?[] StrainID { get; set; }
+        public int?[] DiseaseID { get; set; }
+        public int?[] SubModelID { get; set; }
+        public int?[] RegionID { get; set; }
+        public int?[] SubRegionID { get; set; }
+        public int?[] CellTypeID { get; set; }
+        public int?[] MethodID { get; set; }
+        public int?[] SubMethodID { get; set; }
+        public int?[] TransmitterID { get; set; }
+        public string Reference { get; set; }
+        public string Source { get; set; }
+        public int? YearFrom { get; set; }
+        public int? YearTo { get; set; }
+        public string TaskOther { get; set; }
+        public string SpecieOther { get; set; }
+        public string StrainMouseOther { get; set; }
+        public string StrainRatOther { get; set; }
+        public string DiseaseOther { get; set; }
+        public string CelltypeOther { get; set; }
+        public string MethodOther { get; set; }
+        public string NeurotransOther { get; set; }
+        public string search { get; set; }
 
 
-        public System.Collections.Generic.List<PubScreenAuthor> Author { get; set; } = new List<PubScreenAuthor>();
+        public System.Collections.Generic.List<PubScreenAuthor> Author { get; set; }
 
 
     }
