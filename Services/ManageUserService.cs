d8d0959e4830d872ad4b4fe44d59ed929e2daf2f
mouse-ware <ezareei@uwo.ca>
2025-01-09 12:00:13 -0500

    using async, parameterized sql, added error logging

--- Services/ManageUserService.cs
+++ Services/ManageUserService.cs
@@ -1,9 +1,11 @@
 using AngularSPAWebAPI.Controllers;
 using AngularSPAWebAPI.Models;
 using CBAS.Helpers;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,70 +13,127 @@ namespace AngularSPAWebAPI.Services
 {
     public class ManageUserService
     {
-        public UsersLoginInfo IsUserEmailApproved(string username)
+        public async Task<UsersLoginInfo> IsUserEmailApprovedAsync(string username)
         {
-            bool IsUserApproved = false;
-            bool IsUserLocked = false;
+            bool isUserApproved = false;
+            bool isUserLocked = false;
 
-            string sql = $@"Select EmailConfirmed From AspNetUsers Where UserName = '{username}'";
-            IsUserApproved = Convert.ToBoolean(Dal.ExecScalar(sql).ToString());
+            try
+            {
+                const string sql = @"
+                    SELECT 
+                        EmailConfirmed, 
+                        LockoutEnd 
+                    FROM AspNetUsers 
+                    WHERE UserName = @UserName";
 
-            string sql2 = $@"Select LockoutEnd From AspNetUsers Where UserName = '{username}'";
-            IsUserLocked = string.IsNullOrEmpty(Dal.ExecScalar(sql2).ToString()) ? false : true ;
+                var parameters = new List<SqlParameter>
+                {
+                    new SqlParameter("@UserName", username)
+                };
 
-            return new UsersLoginInfo {
-                IsUserApproved = IsUserApproved,
-                IsUserLocked = IsUserLocked
+                using (var dt = await Dal.GetDataTableAsync(sql, parameters))
+                {
+                    if (dt.Rows.Count > 0)
+                    {
+                        var row = dt.Rows[0];
+                        isUserApproved = row["EmailConfirmed"] != DBNull.Value && Convert.ToBoolean(row["EmailConfirmed"]);
+                        isUserLocked = row["LockoutEnd"] != DBNull.Value && !string.IsNullOrEmpty(row["LockoutEnd"].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error checking if user email is approved for user {Username}", username);
+                throw;
+            }
+
+            return new UsersLoginInfo
+            {
+                IsUserApproved = isUserApproved,
+                IsUserLocked = isUserLocked
             };
         }
 
-        
-        public List<Users> GetAllUserList()
+        public async Task<List<Users>> GetAllUserListAsync()
         {
-            List<Users> lstUser = new List<Users>();
+            var lstUser = new List<Users>();
 
-            using (DataTable dt = Dal.GetDataTable($@"Select AspNetUsers.GivenName, AspNetUsers.FamilyName, AspNetUsers.Email, EmailConfirmed,
-
-                                                        STUFF(( SELECT distinct ' <br/>' + CONCAT(PName, ' - ', Institution) as PISiteName
-                                                        From PISite
-                                                        inner join PI on PI.PID = PISite.PID
-                                                        inner join Site on Site.SiteID = PISite.SiteID
-                                                        inner join PIUserSite on PISite.PSID = PIUserSite.PSID
-                                                        Where PIUserSite.UserID = AspNetUsers.id
-
-                                                        FOR XML PATH(''), type
-                                                        ).value('.', 'nvarchar(max)'),1,6,'') as PISiteName
-
-                                                        From AspNetUsers"))
+            try
             {
-                foreach (DataRow dr in dt.Rows)
-                {
-                    lstUser.Add(new Users
-                    {
-                        GivenName = Convert.ToString(dr["GivenName"].ToString()),
-                        FamilyName = Convert.ToString(dr["FamilyName"].ToString()),
-                        Email = Convert.ToString(dr["Email"].ToString()),
-                        EmailConfirmed =Convert.ToBoolean(dr["EmailConfirmed"].ToString()),
-                        PISiteName = Convert.ToString(dr["PISiteName"].ToString()),
-                        
-                    });
-                }
+                const string sql = @"
+                    SELECT AspNetUsers.GivenName, AspNetUsers.FamilyName, AspNetUsers.Email, EmailConfirmed,
+                    STUFF(( 
+                        SELECT DISTINCT ' <br/>' + CONCAT(PName, ' - ', Institution) AS PISiteName
+                        FROM PISite
+                        INNER JOIN PI ON PI.PID = PISite.PID
+                        INNER JOIN Site ON Site.SiteID = PISite.SiteID
+                        INNER JOIN PIUserSite ON PISite.PSID = PIUserSite.PSID
+                        WHERE PIUserSite.UserID = AspNetUsers.id
+                        FOR XML PATH(''), TYPE
+                    ).value('.', 'nvarchar(max)'), 1, 6, '') AS PISiteName
+                    FROM AspNetUsers";
 
+                using (var dt = await Dal.GetDataTableAsync(sql))
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        lstUser.Add(new Users
+                        {
+                            GivenName = Convert.ToString(dr["GivenName"]),
+                            FamilyName = Convert.ToString(dr["FamilyName"]),
+                            Email = Convert.ToString(dr["Email"]),
+                            EmailConfirmed = Convert.ToBoolean(dr["EmailConfirmed"]),
+                            PISiteName = Convert.ToString(dr["PISiteName"])
+                        });
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error retrieving all user list");
+                throw;
             }
 
             return lstUser;
         }
 
-        public void ApproveSelectedUser(string email)
+        public async Task ApproveSelectedUserAsync(string email)
         {
-            string sql = $@"Update AspNetUsers set EmailConfirmed='true' Where Email = '{email}'";
-            Dal.ExecuteNonQuery(sql);
+            try
+            {
+                const string sql = "UPDATE AspNetUsers SET EmailConfirmed = 'true' WHERE Email = @Email";
+                var parameters = new List<SqlParameter>
+                {
+                    new SqlParameter("@Email", email)
+                };
+
+                await Dal.ExecuteNonQueryAsync(sql, parameters.ToArray());
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error approving selected user with email {Email}", email);
+                throw;
+            }
         }
 
-        public void LockSelectedUser(string email)
+        public async Task LockSelectedUserAsync(string email)
         {
-            string sql = $@"Update AspNetUsers set EmailConfirmed='false' Where Email = '{email}'";
-            Dal.ExecuteNonQuery(sql);
+            try
+            {
+                const string sql = "UPDATE AspNetUsers SET EmailConfirmed = 'false' WHERE Email = @Email";
+                var parameters = new List<SqlParameter>
+                {
+                    new SqlParameter("@Email", email)
+                };
+
+                await Dal.ExecuteNonQueryAsync(sql, parameters.ToArray());
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Error locking selected user with email {Email}", email);
+                throw;
+            }
         }
 
 
