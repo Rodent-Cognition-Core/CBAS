c1a5075d0e7bc9cb4355bc275da13e47e3c585d4
dpalmer9 <dpalmer9@uwo.ca>
2025-01-17 12:04:04 -0500

    Final refactor to reactive form

--- src/app/reqTaskDialoge/reqTaskDialoge.component.ts
+++ src/app/reqTaskDialoge/reqTaskDialoge.component.ts
@@ -24,23 +24,26 @@ export class ReqTaskDialogeComponent implements OnInit {
     faQuestionCircle = faQuestionCircle;
     // Defining Models Parameters
 
-    reqNameModel: string;
-    reqEmailModel: string;
-    reqTaskModel: string;
     reqScheduleModel: string;
            
     private _request = new Request();
 
     // FormControl Parameters
 
-    name = new FormControl('', [Validators.required]);
-    task = new FormControl('', [Validators.required]);
-    email = new FormControl('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")]);
+    name: FormControl;
+    task: FormControl;
+    email: FormControl;
     
 
     constructor(public thisDialogRef: MatDialogRef<ReqTaskDialogeComponent>,
          
-        private requestService: RequestService, ) { }
+        private requestService: RequestService,
+        private fb: FormBuilder) {
+
+        this.name = fb.control('', [Validators.required]);
+        this.task = fb.control('', [Validators.required]);
+        this.email = fb.control('', [Validators.required, Validators.pattern("^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$")]);
+    }
 
     ngOnInit() {
       
@@ -56,9 +59,9 @@ export class ReqTaskDialogeComponent implements OnInit {
     onCloseSubmit(): void {
 
         // building request object
-        this._request.fullName = this.reqNameModel;
-        this._request.email = this.reqEmailModel;
-        this._request.taskName = this.reqTaskModel;
+        this._request.fullName = this.name.value;
+        this._request.email = this.email.value;
+        this._request.taskName = this.task.value;
         this._request.scheduleName = this.reqScheduleModel;
 
         // Submiting the request to server
