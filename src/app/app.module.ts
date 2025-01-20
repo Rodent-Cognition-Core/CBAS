d54b9efb9c325d22ea3b0bf24c54b3cefa68d716
dpalmer9 <dpalmer9@uwo.ca>
2025-01-20 10:45:18 -0500

    Removed EntryComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -161,30 +161,6 @@ const ROUTES: Routes = [
         ScrollService,
 
     ],
-    entryComponents: [
-        ExpDialogeComponent,
-        PubscreenDialogeComponent,
-        AnimalDialogComponent,
-        PasswordDialogComponent,
-        UploadResultDialogComponent,
-        DeleteConfirmDialogComponent,
-        GenericDialogComponent,
-        SubExpDialogeComponent,
-        NotificationDialogComponent,
-        ReqTaskDialogeComponent,
-        ReqPIDialogeComponent,
-        ReqAgeDialogeComponent,
-        ReqGeneralDialogeComponent,
-        ReqPubTaskDialogeComponent,
-        ReqPubModelDialogeComponent,
-        ReqPubSubMethodDialogeComponent,
-        TermsDialogeComponent,
-        ReqMouseLineDialogeComponent,
-        AuthorDialogeComponent,
-        CogbytesDialogueComponent,
-        CogbytesAuthorDialogueComponent,
-        CogbytesPIDialogeComponent,
-    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
