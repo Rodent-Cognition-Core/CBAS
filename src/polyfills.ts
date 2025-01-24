947ef868c48573c8597a2e1e425eafbcd78ec27f
dpalmer9 <dpalmer9@uwo.ca>
2025-01-24 14:18:07 -0500

    Removed references to web-animation-js and protractor

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -46,8 +46,6 @@ import 'core-js/es/reflect';
 /**
  * Required to support Web Animations `@angular/platform-browser/animations`.
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 
