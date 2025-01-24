032948a318af538aebf322c9753c634e72655047
dpalmer9 <dpalmer9@uwo.ca>
2025-01-24 13:16:31 -0500

    Initial core update to 10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,8 +48,9 @@ const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {
-            preloadingStrategy: PreloadAllModules
-        })
+    preloadingStrategy: PreloadAllModules,
+    relativeLinkResolution: 'legacy'
+})
     ],
     exports: [RouterModule]
 })
