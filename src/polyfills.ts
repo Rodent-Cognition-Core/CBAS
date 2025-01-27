2e3aa31fb9c6234a74db217c13817d610770865a
dpalmer9 <dpalmer9@uwo.ca>
2025-01-27 08:34:56 -0500

    Initial migration to 13

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -34,9 +34,6 @@ import 'core-js/es/map';
 import 'core-js/es/weak-map';
 import 'core-js/es/set';
 
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
