73adea7a91dc5f3049fb4c7382d12f9a2949de50
dpalmer9 <dpalmer9@uwo.ca>
2025-01-29 13:34:18 -0500

    Initial migration to Angular 18 core

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // import { FormsModule,  FormControl, Validators, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import {} from '@angular/common/http';
 import { MaterialModule } from './material.module';
 import { SharedExperimentComponent } from '../shared-experiment/shared-experiment.component';
 import { SharedPubscreenComponent } from '../shared-pubscreen/shared-pubscreen.component';
