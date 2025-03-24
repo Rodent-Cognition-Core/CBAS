78e7d502d151ba170923bdf393ed31265f4d0d69
dpalmer9 <dpalmer9@uwo.ca>
2025-03-24 09:17:37 -0400

    Removed relativeLinkResolution parameter

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,8 +48,7 @@ const routes: Routes = [
 @NgModule({
     imports: [
         RouterModule.forRoot(routes, {
-            preloadingStrategy: PreloadAllModules,
-            relativeLinkResolution: 'corrected',
+            preloadingStrategy: PreloadAllModules
         })
     ],
     exports: [RouterModule]
