ee843d520e15c8c639446f3bc15703099f074471
Sujinthan Satkunarajah <Sujinthan@users.noreply.github.com>
2025-07-15 17:36:48 -0400

    Fix identity server (#211)
    * test: trigger GitHub Actions
    
    * uncomment publicOrigin

--- Startup.cs
+++ Startup.cs
@@ -90,11 +90,9 @@ namespace AngularSPAWebAPI
             //Adds serilog to 
             services.AddSerilog();
 
-            if (currentEnvironment.IsProduction())
-            {
-                // Uncomment this line for publuishing
-                services.AddIdentityServer(options =>
-                         options.PublicOrigin = "https://staging.mousebytes.ca")
+            // Uncomment this line for publuishing
+            services.AddIdentityServer(options =>
+                     options.PublicOrigin = "https://staging.mousebytes.ca")
                 //services.AddIdentityServer()
                 // The AddDeveloperSigningCredential extension creates temporary key material for signing tokens.
                 // This might be useful to get started, but needs to be replaced by some persistent key material for production scenarios.
