2180469d00a449fb16bbc793b1f0471b5897b284
Sujinthan Satkunarajah <Sujinthan@users.noreply.github.com>
2025-07-29 13:57:13 -0400

    fix files

--- Program.cs
+++ Program.cs
@@ -30,16 +30,16 @@ namespace AngularSPAWebAPI
                         //.UseUrls("http://*:80;") // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/hosting?tabs=aspnetcore2x
                         //.Build();
                         .UseStartup<Startup>()
-                        //.UseKestrel(o => { o.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10); o.Limits.MaxRequestBodySize = null; })
+                        .UseKestrel(o => { o.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10); o.Limits.MaxRequestBodySize = null; })
                         .UseIIS()
                         .ConfigureKestrel(serverOptions => serverOptions.Listen(IPAddress.Any, 8082, listenOptions =>
                         {
-                            var cert = X509Certificate2.CreateFromPemFile("/https/cloudflare.crt", "/https/cloudflare.key");
+                            var cert = X509Certificate2.CreateFromPemFile("/https/mousebytes-cert.pem", "/https/mousebytes-key.pem");
                             cert = new X509Certificate2(cert.Export(X509ContentType.Pfx));
                             listenOptions.UseHttps(cert);
                         }))
                         //.UseHttpSys(options => { options.MaxRequestBodySize = 100_000_000;})
-                        .UseUrls("https://0.0.0.0:8082")
+                        //.UseUrls("https://0.0.0.0:8082")
                         .Build();
     }
 }
