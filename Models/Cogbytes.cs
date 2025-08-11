a5757d1bfbd9edfbecf6b58161f6b51e1a7c3582
dpalmer9 <dpalmer9@uwo.ca>
2025-08-11 09:42:21 -0400

    Added metadata to backend Model

--- Models/Cogbytes.cs
+++ Models/Cogbytes.cs
@@ -17,6 +17,32 @@ namespace AngularSPAWebAPI.Models
         public int?[] PIID { get; set; }
         public string PIString { get; set; }
         public string Link { get; set; }
+        public int?[] TaskID { get; set; }
+        public int?[] SubTaskID { get; set; }
+        public int?[] SpecieID { get; set; }
+        public int?[] SexID { get; set; }
+        public int?[] StrainID { get; set; }
+        public int?[] GenoID { get; set; }
+        public int?[] DiseaseID { get; set; }
+        public int?[] SubModelID { get; set; }
+        public int?[] RegionID { get; set; }
+        public int?[] SubRegionID { get; set; }
+        public int?[] CellTypeID { get; set; }
+        public int?[] MethodID { get; set; }
+        public int?[] SubMethodID { get; set; }
+        public int?[] TransmitterID { get; set; }
+        public int? YearFrom { get; set; }
+        public int? YearTo { get; set; }
+        public int? StartAge { get; set; }
+        public int? EndAge { get; set; }
+        public string TaskOther { get; set; }
+        public string SpecieOther { get; set; }
+        public string StrainMouseOther { get; set; }
+        public string StrainRatOther { get; set; }
+        public string DiseaseOther { get; set; }
+        public string CelltypeOther { get; set; }
+        public string MethodOther { get; set; }
+        public string NeurotransOther { get; set; }
         public bool PrivacyStatus { get; set; }
         public string Description { get; set; }
         public string AdditionalNotes { get; set; }
