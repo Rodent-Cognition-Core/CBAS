f18761fe8cadc53cc342e5500376379e11a27ae8
Sujinthan Satkunarajah <Sujinthan@users.noreply.github.com>
2025-11-11 13:56:08 -0500

    add proxy url

---
+++ proxy.production.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "/api": {
+        target: "https://mousebytes.ca",
+        secure: false,
+        changeOrigin: true,
+        logLevel: "debug"
+    },
+    "/connect": {
+        target: "https://mousebytes.ca",
+        secure: false,
+        changeOrigin: true,
+        logLevel: "debug"
+    }
+};
